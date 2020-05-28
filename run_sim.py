6dce82e3e484eee50202e382c2f5f69afb14f781
Gleb Sterkin <31451813+belkakari@users.noreply.github.com>
2020-05-28 22:24:53 +0300

    Update run_sim.py

--- run_sim.py
+++ run_sim.py
@@ -1,7 +1,7 @@
 import taichi as ti
 import numpy as np
 
-from .ball_pytorch import RollingBallPytorchSim
+from ball_pytorch import RollingBallPytorchSim
 
 
 ti.init(arch=ti.cpu, default_fp=ti.f32)
